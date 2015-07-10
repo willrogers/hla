ab5943aea5826ade5e8e9dec37c5a411ef69fffe
Will Rogers <wahrogers@gmail.com>
2015-07-10 13:39:15 +0100

    Add load_mml.m.
    This requires a list of BPMs.
    Also include Matlab sqlite library for convenience.

---
+++ utils.dls/load_mml.m
@@ -0,0 +1,156 @@
+function load_mml(ringmode)
+
+    fprintf('Loading data for ring mode %s', ringmode);
+    dir = fileparts(mfilename('fullpath'));
+    cd(dir);
+
+    loaded_mode = getfamilydata('OperationalMode');
+
+    if ~strcmp(loaded_mode, ringmode)
+        fprintf('MML ring mode %s loaded, not %s\n', loaded_mode, ringmode);
+        fprintf('Exiting.\n');
+        return;
+    end
+
+    % load directly into the ap SQL database
+    machine_dir = fullfile(dir, '..', 'aphla', 'machines', ringmode);
+
+    % mksqlite library in this repository for now.
+    addpath('./mksqlite');
+    DB_FILE = fullfile(machine_dir, 'data.sqlite3');
+
+    mksqlite('open', DB_FILE);
+    mksqlite('delete from elements');
+    mksqlite('delete from pvs');
+
+    global THERING;
+    ao = getao();
+
+    % The individual BPM PVs are not stored in middlelayer.
+    global BPMS;
+    BPMS = load_bpm_file();
+
+    % Map from AT types to types in the accelerator object (ao).
+    global TYPE_MAP;
+    keys = {'QUAD', 'SEXT', 'VSTR', 'HSTR'};
+    values = {'QUAD_', 'SEXT_', 'VCM', 'HCM'};
+    TYPE_MAP = containers.Map(keys, values);
+
+    usedElements = containers.Map();
+
+    s = 0;
+
+    for i = 1:length(THERING)
+        disp(i);
+        elm = THERING{i};
+        s = s + elm.Length;
+        insertelement(i, elm, s);
+
+        type = gettype(elm);
+        if usedElements.isKey(type)
+            usedElements(type) = usedElements(type) + 1;
+        else
+            usedElements(type) = 1;
+        end
+        pvs = getpvs(ao, elm, usedElements);
+        insertpvs(i, pvs);
+    end
+
+    % DCCT not in THERING.
+    dcct = struct ('FamName', 'DCCT', 'Length', 0);
+    i = i + 1;
+    insertelement(i, dcct, 0);
+    s = pv_struct('SR-DI-DCCT-01:SIGNAL', 'I', 'get');
+    insertpvs(i, {s});
+
+    mksqlite('close');
+
+end
+
+
+function type = gettype(elm)
+    if isfield(elm, 'Class')
+        type = elm.Class;
+    elseif isfield(elm, 'FamName')
+            type = elm.FamName;
+    else
+        type = '';
+    end
+end
+
+
+function insertpvs(index, pvs)
+    PV_INSERT = 'insert into pvs (pv, elemName, elemField, elemHandle) values (?,?,?,?)';
+    for i = 1:length(pvs)
+        pv = pvs{i};
+        mksqlite(PV_INSERT, pv.pv, num2str(index), pv.field, pv.handle);
+    end
+
+end
+
+function bpms = load_bpm_file()
+    BPMS_FILE = 'data/bpms.txt';
+    fileID = fopen(BPMS_FILE, 'r');
+    formatSpec = '%s';
+    all_bpms = textscan(fileID, formatSpec);
+    fclose(fileID);
+    bpms = all_bpms{1};
+end
+
+
+function pvs = getpvs(ao, elm, usedElements)
+
+    global BPMS;
+    global TYPE_MAP;
+
+    type = gettype(elm);
+
+    if strcmp(type, 'QUAD') || strcmp(type, 'SEXT') || strcmp(type, 'HSTR') || strcmp(type, 'VSTR')
+        index = usedElements(type);
+        family = TYPE_MAP(type);
+        get_pv = ao.(family).Monitor.ChannelNames(index, :);
+        gpv = pv_struct(get_pv, 'b1', 'get');
+        set_pv = ao.(family).Setpoint.ChannelNames(index, :);
+        spv = pv_struct(set_pv, 'b1', 'put');
+        pvs = {gpv, spv};
+    elseif strcmp(type, 'BPM')
+        index = usedElements(type);
+        get_x_pv = strcat(BPMS{index}, ':SA:X');
+        x_pv = pv_struct(get_x_pv, 'x', 'get');
+        get_y_pv = strcat(BPMS{index}, ':SA:Y');
+        y_pv = pv_struct(get_y_pv, 'y', 'get');
+        pvs = {x_pv, y_pv};
+    elseif strcmp(type, 'RF')
+        gfpv = ao.(type).Monitor.ChannelNames;
+        get_f_pv = pv_struct(gfpv, 'f', 'get');
+        sfpv = ao.(type).Setpoint.ChannelNames;
+        set_f_pv = pv_struct(sfpv, 'f', 'put');
+        % voltage?
+        pvs = {get_f_pv, set_f_pv};
+    else
+        pvs = {};
+    end
+
+end
+
+
+function s = pv_struct(pv, field, handle)
+    s = struct('pv', pv, 'field', field, 'handle', handle);
+end
+
+function insertelement(i, elm, s)
+    INSERT_ELEMENT = 'insert into elements (elemName, elemLength, elemPosition, elemIndex, elemType, elemGroups, k1, k2, virtual) values (?,?,?,?,?,?,?,?,?)';
+    k1 = 0;
+    k2 = 0;
+    type = gettype(elm);
+
+    if strcmp(type, 'QUAD')
+        k1 = elm.K;
+        k2 = 0;
+    elseif strcmp(type, 'SEXT')
+        k2 = elm.PolynomB(3);
+        k1 = 0;
+    end
+    mksqlite(INSERT_ELEMENT, num2str(i), elm.Length, s, num2str(i), type, elm.FamName, k1, k2, 0);
+end
+
